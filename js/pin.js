29aa2017bbdb0d9e7ae34dc55c3096acc39e2730
blizzardKv <makemewanna@yandex.ru>
2020-06-21 22:15:07 +0300

    выпилена вторая часть задания модуля, относится ко второй части, ошибся при мерже из мастера

--- js/pin.js
+++ js/pin.js
@@ -39,62 +39,6 @@
         fragment.appendChild(window.pin.setPinOptions(pinsList[i]));
       }
       window.domComponents.mapPinsArea.appendChild(fragment);
-    },
-
-    moveMainPin: function () {
-      // Зажимаем пин, добавляем слушателя
-      mainPin.addEventListener('mousedown', function (evt) {
-        evt.preventDefault();
-
-        // Записываем дефолтные координаты
-        var startCoordinates = {
-          x: evt.clientX,
-          y: evt.clientY
-        };
-
-        // Добавляем коллбэк на перетаскивание элемента
-        var onMouseMove = function (moveEvt) {
-          moveEvt.preventDefault();
-
-          // Получаем сдвиг относительно от начальных координат.
-          var shift = {
-            x: startCoordinates.x - moveEvt.clientX,
-            y: startCoordinates.y - moveEvt.clientY
-          };
-
-          // Перезаписываем стартовые координаты, иначе наш сдвиг будет бесконечно скакать.
-          startCoordinates = {
-            x: moveEvt.clientX,
-            y: moveEvt.clientY
-          };
-
-          // Рассчитываем значения координаты x и y согласно ограничений. На входе имеем - math.max - воозвращаем максимальное значение из массива значений -
-          // либо x.MIN, либо минимальное из двух значений mainPin.offsetLeft - shift.x и window.data.pinArea.x.MAX. Аналогично по оси Y.
-          mainPin.coordsX = Math.floor(Math.max(window.data.pinArea.x.MIN, Math.min(mainPin.offsetLeft - shift.x, window.data.pinArea.x.MAX))) + 'px';
-          mainPin.coordsY = Math.floor(Math.max(window.data.pinArea.y.MIN, Math.min(mainPin.offsetTop - shift.y, window.data.pinArea.y.MAX))) + 'px';
-
-          // Задаем значение по осям X, Y - согласно вычислениям.
-          mainPin.style.left = mainPin.coordsX;
-          mainPin.style.top = mainPin.coordsY;
-
-          // Тут будет отрабатывать при каждом сдвиге относительно координат, если перенести
-          // в onMouseUp - то только по отпусканию кнопки мыши.
-          window.domComponents.addressInput.value = (mainPin.style.left + '; ' + mainPin.style.top);
-        };
-
-        // На поднятии клавиши мышки - снимаем слушаетелей.
-        var onMouseUp = function (upEvt) {
-          upEvt.preventDefault();
-
-          document.removeEventListener('mousemove', onMouseMove);
-          document.removeEventListener('mouseup', onMouseUp);
-        };
-
-        // Выполняем реинит слушателей, иначе пин после первого отпускания кнопки мыши
-        // не будет больше перетаскивать
-        document.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-      });
     }
   };
 
